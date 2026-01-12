55f100b4e6602601131aa3a9525b255db199b094
boticelli <kandelakitina@gmail.com>
2026-01-12 14:13:53 +0300

    Add simple args parsing

--- main.ts
+++ main.ts
@@ -1,1 +1,11 @@
-console.log(Deno.env.get("OMDB_API_KEY"));
+import { parseArgs } from "@std/cli/parse-args";
+
+// const OMDB_API_KEY = Deno.env.get("OMDB_API_KEY");
+
+// Args Parsing
+
+const args = parseArgs(Deno.args);
+
+const movie = args._[0];
+
+console.log(movie);
